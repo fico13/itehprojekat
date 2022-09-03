1f20a8b0c7e5fcd98c7ae25ff6f1deb5e3833f19
fico13 <ficojovanovic13@gmail.com>
2022-09-03 14:50:23 +0200

    modified:   backend/app.js 	new file:   backend/controllers/blog-controller.js 	modified:   backend/controllers/user-controller.js 	new file:   backend/model/Blog.js 	modified:   backend/model/User.js 	new file:   backend/routes/blog-routes.js

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import blogRouter from "./routes/blog-routes";
 import router from "./routes/user-routes";
 
 
@@ -7,6 +8,7 @@ const app  = express();
 
 app.use(express.json());
 app.use("/api/user",router);
+app.use("/api/blog", blogRouter);
 
 mongoose.connect('mongodb+srv://admin:RX5YEwikOqsQh4fL@cluster0.blxayut.mongodb.net/Blog?retryWrites=true&w=majority')
 .then(() => app.listen(5000))
