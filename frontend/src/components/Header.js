b6cf7d8a5171cb2b73b6243fc54746f18c88ed56
fico13 <ficojovanovic13@gmail.com>
2022-09-04 10:27:26 +0200

    kostur za backend

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {AppBar, Toolbar, Typography} from "@mui/material";
+
+const Header = () => {
+  return (
+    <AppBar>
+        <Toolbar>
+            <Typography>
+            BlogApp
+            </Typography>
+        </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Header
